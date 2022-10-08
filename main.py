d59e2ab2e6c570bfa6245735a49731b23d108e20
mikey <mike.owino40@yahoo.com>
2022-10-08 12:44:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Darkweb Login")
+username = input("Enter username? ")
+print("")
+password = input("Password? ")
+print("")
+
+if username == "root" and password == "PyTh0nR*cks":
+    print("Welcome, admin!!")
+elif username == "Becky" and password == "Repl!t4evEr":
+    print("Hi Becky! Love your hair today!")
+elif username == "Bill" and password == "SmashTHEb*gs!":
+    print("Yo! Bill! What up?!")
+else:
+    print("You do not have access. Go away!")
